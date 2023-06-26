d689f7f1210a30ea1ac47487056a0710abee3747
altinhykolli <altinhykolli@gmail.comm>
2023-06-27 01:55:01 +0200

    module change v.1.0.0 to v1.0.1

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,7 @@
 module "ec2" {
   source  = "app.terraform.io/altin-devops/ec2/aws"
-  version = "1.0.0"
+  version = "1.0.1"
+  name = "web-server-altin"
+  instance_type = "t2.micro"
   # insert required variables here
 }
